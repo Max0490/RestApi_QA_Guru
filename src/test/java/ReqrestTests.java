4304d4aeec8143e1f3d61a3cebe64c1298fb9b78
USER <seagate90@mail.ru>
2022-11-22 19:05:31 +0400

    Initial commit

---
+++ src/test/java/ReqrestTests.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class ReqrestTests {
+
+    @Test
+    void singleUser() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"));
+    }
+
+    void singleUserNotFound() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .statusCode(404);
+    }
+
+    void singleResource() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.name", is("fuchsia rose"))
+                .body("data.year", is("2001"))
+                .body("data.color", is("#C74375"))
+                .body("pantone_value", is("17-2031"));
+    }
+
+    void singleResourseNotFound() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown/23")
+                .then()
+                .log().status()
+                .statusCode(404);
+    }
+
+    void listResourse() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(1))
+                .body("data.name", is("cerulean"))
+                .body("data.year", is("2000"))
+                .body("data.color", is("#98B2D1"))
+                .body("pantone_value", is("15-4020"));
+    }
+
+}
